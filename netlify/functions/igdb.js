174abe299e071763e2e5ab1374714e5b2acdb7f6
unknown <yolosid167@gmail.com>
2026-03-13 08:46:38 +1000

    Add live IGDB search with dropdown

--- netlify/functions/igdb.js
+++ netlify/functions/igdb.js
@@ -23,7 +23,10 @@ exports.handler = async (event) => {
 
   return {
     statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
     body: JSON.stringify(data)
   };
 };
